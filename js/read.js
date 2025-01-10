e517d51f0d3769fc516aac53aae9956e634c3db1
Simran29 <simranbodale2912@gmail.com>
2025-01-10 14:16:41 +0530

    read.html css and js file separated

--- js/read.js
+++ js/read.js
@@ -1245,3 +1245,43 @@ fetchLanguages()
             });
         });
     });
+
+    //--------------------------------------------------------------------
+    $(document).on('ready', function () {
+        $(".vertical-center-4").slick({
+            dots: false,
+            infinite: true,
+            slidesToShow: 4,
+            slidesToScroll: 2,
+            centerMode: true,
+            autoplay: true,
+            autoplaySpeed: 3000,
+            arrows: true,
+            responsive: [
+                {
+                    breakpoint: 1024,
+                    settings: {
+                        slidesToShow: 3,
+                        slidesToScroll: 1,
+                        infinite: true,
+                        dots: true,
+                    }
+                },
+                {
+                    breakpoint: 768,
+                    settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 1,
+                    }
+                },
+                {
+                    breakpoint: 480,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1,
+                    }
+                }
+            ]
+        });
+      
+    });
